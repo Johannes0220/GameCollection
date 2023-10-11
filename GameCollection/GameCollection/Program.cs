b68007a8a734444ba19edcc0b004e319ad2ed6c6
Johannes <johannes.schmaelzle@web.de>
2023-10-11 10:21:34 +0200

    Initial Project

---
+++ GameCollection/GameCollection/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Initial Commit");
+        var me=Console.ReadLine();
+        Console.WriteLine(me);
+        Console.Read();
+    }
+}
