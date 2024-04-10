b91bf9ee617feae694c20e3f240dfe30759c6a04
Bäuml, Thomas <baeuml@dhbw-loerrach.de>
2024-04-10 11:22:47 +0200

    Implement hangman (need to fix: separate Render and DeatchAnimation to another class, delay DeatchAnimation, last console read and write need to go from Controller to View)

---
+++ GameCollection/GameCollection/Games/Hangman/Hangman.cs
@@ -0,0 +1,45 @@
+﻿using GameCollection.Games.Hangman;
+
+namespace GameCollection.Games.Hangman;
+public class HangmanGame
+{
+    private readonly string[] _wordArray = { "hangman", "game", "rainbow", "game", "street", "germany"};
+    private readonly string _randomWord;
+    private readonly char[] _revealedChars;
+    public int _incorrectGuesses;
+
+    public HangmanGameState State { get; private set; }
+
+    public HangmanGame()
+    {
+        _randomWord = _wordArray[new Random().Next(_wordArray.Length)].ToLower();
+        _revealedChars = new string('_', _randomWord.Length).ToCharArray();
+        State = HangmanGameState.InProgress;
+    }
+
+    public void Guess(char guess)
+    {
+        bool correctGuess = false;
+        for (int i = 0; i < _revealedChars.Length; i++)
+        {
+            if (_revealedChars[i] == '_' && _randomWord[i] == guess)
+            {
+                _revealedChars[i] = guess;
+                correctGuess = true;
+            }
+        }
+        if (!correctGuess)
+        {
+            _incorrectGuesses++;
+            if (_incorrectGuesses >= 6)
+                State = HangmanGameState.Lost;
+        }
+        if (!_revealedChars.Contains('_'))
+            State = HangmanGameState.Won;
+    }
+
+    public string GetRevealedWord()
+    {
+        return new string(_revealedChars);
+    }
+}
---
+++ GameCollection/GameCollection/Games/Hangman/HangmanController.cs
@@ -0,0 +1,41 @@
+﻿using GameCollection.Games;
+using GameCollection.Games.Hangman;
+
+namespace GameCollection.Games.Hangman;
+
+public class HangmanGameController:IPlayable
+{
+    private readonly HangmanGame _hangmanGame;
+    private readonly HangmanGameView _hangmanView;
+    private readonly string _name = "Hangman";
+    private readonly Guid _guid = Guid.NewGuid();
+
+    public HangmanGameController()
+    {
+        _hangmanGame = new HangmanGame();
+        _hangmanView = new HangmanGameView();
+    }
+
+    public void StartGame()
+    {
+        while (_hangmanGame.State == HangmanGameState.InProgress)
+        {
+            string currentWord = _hangmanGame.GetRevealedWord();
+            HangmanGameState state = _hangmanGame.State;
+            int incorrectGuesses = _hangmanGame._incorrectGuesses;
+
+            _hangmanView.RenderGameState(currentWord, state, incorrectGuesses);
+
+            Console.WriteLine("Enter your guess (a-z):");
+            char guess = Console.ReadKey().KeyChar;
+            _hangmanGame.Guess(guess);
+        }
+
+        _hangmanView.RenderGameState(_hangmanGame.GetRevealedWord(), _hangmanGame.State, _hangmanGame._incorrectGuesses);
+    }
+
+    public string getName()
+    {
+        return _name;
+    }
+}
