f290529fc4aaec3697bf1c6300cf13f2db5709eb
Johannes <johannes.schmaelzle@web.de>
2024-05-03 10:16:52 +0200

    Added UserrepoMock & UserServiceTests

---
+++ GameCollection/Test/User/UserRepositoryMock.cs
@@ -0,0 +1,43 @@
+﻿using GameCollection.User;
+
+namespace Test.User;
+
+public class UserRepositoryMock:IUserRepository
+{
+
+    private Dictionary<Guid, GameCollection.User.User> _users;
+
+    public UserRepositoryMock()
+    {
+        _users = new Dictionary<Guid, GameCollection.User.User>();
+    }
+    public List<GameCollection.User.User> GetAllUsers()
+    {
+        var users = _users.Values;
+
+
+        return users.Cast<GameCollection.User.User>().ToList(); ;
+    }
+
+    public GameCollection.User.User GetUserById(Guid id)
+    {
+        return _users[id];
+    }
+
+    public GameCollection.User.User CreateUser(string name)
+    {
+        var user = new GameCollection.User.User(Guid.NewGuid(), name);
+        _users.Add(user.Id, user);
+        return user;
+    }
+
+    public GameCollection.User.User UpdateUser(GameCollection.User.User user)
+    {
+        if (GetUserById(user.Id) is not null)
+        {
+            _users.Remove(user.Id);
+            _users.Add(user.Id, user);
+        }
+        return user;
+    }
+}
---
+++ GameCollection/Test/User/UserServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using GameCollection.User;
+
+namespace Test.User;
+
+[TestClass]
+public class UserServiceTest
+{
+
+    [TestMethod]
+    public void TestCreateUser()
+    {
+        //Arrange
+        var service = new UserService(new UserRepositoryMock());
+        //Act
+        var user = service.CreateUser("TestUser");
+        //Assert
+        Assert.AreEqual(user.Name, "TestUser");
+    }
+
+    [TestMethod]
+    public void TestUpdateUser()
+    {
+        //Arrange
+        var service = new UserService(new UserRepositoryMock());
+        var user = service.CreateUser("TestUser");
+        //Act
+        user.Name = "TestUser2";
+        var updatedUser=service.UpdateUser(user);
+        //Assert
+        Assert.AreEqual(updatedUser.Name, "TestUser2");
+    }
+
+}
