a9312c59d709e41a237c67f98a06e1750b44cd1d
Bäuml, Thomas <baeuml@dhbw-loerrach.de>
2024-05-05 17:19:12 +0200

    main menu changes

--- GameCollection/GameCollection/Views/MainMenuView.cs
+++ GameCollection/GameCollection/Views/MainMenuView.cs
@@ -7,9 +7,10 @@ public class MainMenuView: BasicView
     public int ShowMainMenu()
     {
         Console.WriteLine("What would you like to do?");
-        Console.WriteLine("[0] Play a Game");
-        Console.WriteLine("[1] Check Archivments");
-        var input = ReadNumericInput("Enter a number form the List!", 0, 1);
+        Console.WriteLine("0. Play a Game");
+        Console.WriteLine("1. Check Archivments");
+        var input = ReadNumericInput("Enter a number form the List: ", 0, 1);
+        Console.Clear();
         return input;
     }
 }
