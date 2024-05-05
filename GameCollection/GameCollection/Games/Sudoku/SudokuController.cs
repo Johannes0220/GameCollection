79231bd7470d99bfae71d3f5db7432a9eeb77c59
Johannes <johannes.schmaelzle@web.de>
2024-05-05 21:57:10 +0200

    Fix Code Smell Long Method

--- GameCollection/GameCollection/Games/Sudoku/SudokuController.cs
+++ GameCollection/GameCollection/Games/Sudoku/SudokuController.cs
@@ -39,78 +39,7 @@ public class SudokuController:IPlayable
 
                 var key = Console.ReadKey(true).Key;
 
-                switch (key)
-                {
-                    case ConsoleKey.UpArrow:
-                        x = (x <= 0) ? 8 : x - 1;
-                        break;
-                    case ConsoleKey.DownArrow:
-                        x = (x >= 8) ? 0 : x + 1;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        y = (y <= 0) ? 8 : y - 1;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        y = (y >= 8) ? 0 : y + 1;
-                        break;
-                    case ConsoleKey.D1:
-                    case ConsoleKey.NumPad1:
-                        if (_sudoku.IsValidMove(1, x, y))
-                            activeBoard[x, y] = 1;
-                        break;
-                    case ConsoleKey.D2:
-                    case ConsoleKey.NumPad2:
-                        if (_sudoku.IsValidMove(2, x, y))
-                            activeBoard[x, y] = 2;
-                        break;
-                    case ConsoleKey.D3:
-                    case ConsoleKey.NumPad3:
-                        if (_sudoku.IsValidMove(3, x, y))
-                            activeBoard[x, y] = 3;
-                        break;
-                    case ConsoleKey.D4:
-                    case ConsoleKey.NumPad4:
-                        if (_sudoku.IsValidMove(4, x, y))
-                            activeBoard[x, y] = 4;
-                        break;
-                    case ConsoleKey.D5:
-                    case ConsoleKey.NumPad5:
-                        if (_sudoku.IsValidMove(5, x, y))
-                            activeBoard[x, y] = 5;
-                        break;
-                    case ConsoleKey.D6:
-                    case ConsoleKey.NumPad6:
-                        if (_sudoku.IsValidMove(6, x, y))
-                            activeBoard[x, y] = 6;
-                        break;
-                    case ConsoleKey.D7:
-                    case ConsoleKey.NumPad7:
-                        if (_sudoku.IsValidMove(7, x, y))
-                            activeBoard[x, y] = 7;
-                        break;
-                    case ConsoleKey.D8:
-                    case ConsoleKey.NumPad8:
-                        if (_sudoku.IsValidMove(8, x, y))
-                            activeBoard[x, y] = 8;
-                        break;
-                    case ConsoleKey.D9:
-                    case ConsoleKey.NumPad9:
-                        if (_sudoku.IsValidMove(9, x, y))
-                            activeBoard[x, y] = 9;
-                        break;
-
-                    case ConsoleKey.Backspace:
-                    case ConsoleKey.Delete:
-                        activeBoard[x, y] = generatedBoard[x, y];
-                        break;
-
-                    case ConsoleKey.End:
-                        _gameState = SudokuGameState.Ongoing; 
-                        break;
-                    case ConsoleKey.Escape:
-                        _gameState = SudokuGameState.CloseRequested; 
-                        break;
-                }
+                x = EvaluateInput(key, x, activeBoard, generatedBoard, ref y);
 
                 if (!_sudoku.ContainsNulls())
                 {
@@ -126,6 +55,84 @@ public class SudokuController:IPlayable
 
         _view.DisplayGameOverMesssage();
         return new WinGameResult(true);
+    }
+
+    private int EvaluateInput(ConsoleKey key, int x, int?[,] activeBoard, int?[,] generatedBoard, ref int y)
+    {
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+                x = (x <= 0) ? 8 : x - 1;
+                break;
+            case ConsoleKey.DownArrow:
+                x = (x >= 8) ? 0 : x + 1;
+                break;
+            case ConsoleKey.LeftArrow:
+                y = (y <= 0) ? 8 : y - 1;
+                break;
+            case ConsoleKey.RightArrow:
+                y = (y >= 8) ? 0 : y + 1;
+                break;
+            case ConsoleKey.D1:
+            case ConsoleKey.NumPad1:
+                if (_sudoku.IsValidMove(1, x, y))
+                    activeBoard[x, y] = 1;
+                break;
+            case ConsoleKey.D2:
+            case ConsoleKey.NumPad2:
+                if (_sudoku.IsValidMove(2, x, y))
+                    activeBoard[x, y] = 2;
+                break;
+            case ConsoleKey.D3:
+            case ConsoleKey.NumPad3:
+                if (_sudoku.IsValidMove(3, x, y))
+                    activeBoard[x, y] = 3;
+                break;
+            case ConsoleKey.D4:
+            case ConsoleKey.NumPad4:
+                if (_sudoku.IsValidMove(4, x, y))
+                    activeBoard[x, y] = 4;
+                break;
+            case ConsoleKey.D5:
+            case ConsoleKey.NumPad5:
+                if (_sudoku.IsValidMove(5, x, y))
+                    activeBoard[x, y] = 5;
+                break;
+            case ConsoleKey.D6:
+            case ConsoleKey.NumPad6:
+                if (_sudoku.IsValidMove(6, x, y))
+                    activeBoard[x, y] = 6;
+                break;
+            case ConsoleKey.D7:
+            case ConsoleKey.NumPad7:
+                if (_sudoku.IsValidMove(7, x, y))
+                    activeBoard[x, y] = 7;
+                break;
+            case ConsoleKey.D8:
+            case ConsoleKey.NumPad8:
+                if (_sudoku.IsValidMove(8, x, y))
+                    activeBoard[x, y] = 8;
+                break;
+            case ConsoleKey.D9:
+            case ConsoleKey.NumPad9:
+                if (_sudoku.IsValidMove(9, x, y))
+                    activeBoard[x, y] = 9;
+                break;
+
+            case ConsoleKey.Backspace:
+            case ConsoleKey.Delete:
+                activeBoard[x, y] = generatedBoard[x, y];
+                break;
+
+            case ConsoleKey.End:
+                _gameState = SudokuGameState.Ongoing;
+                break;
+            case ConsoleKey.Escape:
+                _gameState = SudokuGameState.CloseRequested;
+                break;
+        }
+
+        return x;
     }
 
     public string GetName()
